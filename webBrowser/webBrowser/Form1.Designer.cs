2803509868fee88a92036ed8fe49660e804c6cf1
Strupervlg <53981215+Strupervlg@users.noreply.github.com>
2021-07-04 13:21:09 +0300

    Добавлена кнопка клика и исправлен ресайз лейаута

--- webBrowser/webBrowser/Form1.Designer.cs
+++ webBrowser/webBrowser/Form1.Designer.cs
@@ -44,6 +44,7 @@ namespace webBrowser
             this.buttonPlusRate = new System.Windows.Forms.Button();
             this.buttonMinusRate = new System.Windows.Forms.Button();
             this.buttonKeyboard = new System.Windows.Forms.Button();
+            this.buttonClickMouse = new System.Windows.Forms.Button();
             this.mainTableLayoutPanel.SuspendLayout();
             this.groupBoxVolume.SuspendLayout();
             this.tableLayoutPanelVolume.SuspendLayout();
@@ -58,7 +59,7 @@ namespace webBrowser
             this.buttonPausePlay.Dock = System.Windows.Forms.DockStyle.Fill;
             this.buttonPausePlay.Location = new System.Drawing.Point(3, 3);
             this.buttonPausePlay.Name = "buttonPausePlay";
-            this.buttonPausePlay.Size = new System.Drawing.Size(180, 79);
+            this.buttonPausePlay.Size = new System.Drawing.Size(192, 94);
             this.buttonPausePlay.TabIndex = 1;
             this.buttonPausePlay.Text = "Pause/Play";
             this.buttonPausePlay.UseVisualStyleBackColor = true;
@@ -68,25 +69,26 @@ namespace webBrowser
             // 
             // browser
             // 
-            this.mainTableLayoutPanel.SetColumnSpan(this.browser, 6);
+            this.mainTableLayoutPanel.SetColumnSpan(this.browser, 7);
             this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.browser.Location = new System.Drawing.Point(0, 85);
+            this.browser.Location = new System.Drawing.Point(0, 100);
             this.browser.Margin = new System.Windows.Forms.Padding(0);
             this.browser.MinimumSize = new System.Drawing.Size(20, 20);
             this.browser.Name = "browser";
-            this.browser.Size = new System.Drawing.Size(1450, 569);
+            this.browser.Size = new System.Drawing.Size(1542, 569);
             this.browser.TabIndex = 0;
             this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.browser_DocumentCompleted);
             // 
             // mainTableLayoutPanel
             // 
-            this.mainTableLayoutPanel.ColumnCount = 6;
-            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 186F));
-            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 197F));
-            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 364F));
-            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 309F));
-            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 194F));
+            this.mainTableLayoutPanel.ColumnCount = 7;
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.87641F));
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.8764F));
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.8764F));
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 21.51926F));
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.0955F));
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.8764F));
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.87963F));
             this.mainTableLayoutPanel.Controls.Add(this.buttonNext, 2, 0);
             this.mainTableLayoutPanel.Controls.Add(this.browser, 0, 1);
             this.mainTableLayoutPanel.Controls.Add(this.buttonPausePlay, 0, 0);
@@ -94,23 +96,23 @@ namespace webBrowser
             this.mainTableLayoutPanel.Controls.Add(this.groupBoxVolume, 3, 0);
             this.mainTableLayoutPanel.Controls.Add(this.groupBoxRate, 4, 0);
             this.mainTableLayoutPanel.Controls.Add(this.buttonKeyboard, 5, 0);
+            this.mainTableLayoutPanel.Controls.Add(this.buttonClickMouse, 6, 0);
             this.mainTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
             this.mainTableLayoutPanel.Name = "mainTableLayoutPanel";
             this.mainTableLayoutPanel.RowCount = 2;
-            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 72.56637F));
-            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 569F));
-            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.mainTableLayoutPanel.Size = new System.Drawing.Size(1450, 654);
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.mainTableLayoutPanel.Size = new System.Drawing.Size(1542, 669);
             this.mainTableLayoutPanel.TabIndex = 0;
             // 
             // buttonNext
             // 
             this.buttonNext.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.buttonNext.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonNext.Location = new System.Drawing.Point(389, 3);
+            this.buttonNext.Location = new System.Drawing.Point(399, 3);
             this.buttonNext.Name = "buttonNext";
-            this.buttonNext.Size = new System.Drawing.Size(191, 79);
+            this.buttonNext.Size = new System.Drawing.Size(192, 94);
             this.buttonNext.TabIndex = 1;
             this.buttonNext.Text = "Next";
             this.buttonNext.UseVisualStyleBackColor = true;
@@ -122,9 +124,9 @@ namespace webBrowser
             // 
             this.buttonBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.buttonBack.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonBack.Location = new System.Drawing.Point(189, 3);
+            this.buttonBack.Location = new System.Drawing.Point(201, 3);
             this.buttonBack.Name = "buttonBack";
-            this.buttonBack.Size = new System.Drawing.Size(194, 79);
+            this.buttonBack.Size = new System.Drawing.Size(192, 94);
             this.buttonBack.TabIndex = 2;
             this.buttonBack.Text = "Back";
             this.buttonBack.UseVisualStyleBackColor = true;
@@ -136,9 +138,9 @@ namespace webBrowser
             // 
             this.groupBoxVolume.Controls.Add(this.tableLayoutPanelVolume);
             this.groupBoxVolume.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBoxVolume.Location = new System.Drawing.Point(586, 3);
+            this.groupBoxVolume.Location = new System.Drawing.Point(597, 3);
             this.groupBoxVolume.Name = "groupBoxVolume";
-            this.groupBoxVolume.Size = new System.Drawing.Size(358, 79);
+            this.groupBoxVolume.Size = new System.Drawing.Size(325, 94);
             this.groupBoxVolume.TabIndex = 3;
             this.groupBoxVolume.TabStop = false;
             this.groupBoxVolume.Text = "Volume";
@@ -146,9 +148,9 @@ namespace webBrowser
             // tableLayoutPanelVolume
             // 
             this.tableLayoutPanelVolume.ColumnCount = 3;
-            this.tableLayoutPanelVolume.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanelVolume.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 122F));
-            this.tableLayoutPanelVolume.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 115F));
+            this.tableLayoutPanelVolume.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanelVolume.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanelVolume.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanelVolume.Controls.Add(this.buttonPlusVolume, 1, 0);
             this.tableLayoutPanelVolume.Controls.Add(this.buttonMinusVolume, 0, 0);
             this.tableLayoutPanelVolume.Controls.Add(this.buttonMuteVolume, 2, 0);
@@ -156,18 +158,17 @@ namespace webBrowser
             this.tableLayoutPanelVolume.Location = new System.Drawing.Point(3, 16);
             this.tableLayoutPanelVolume.Name = "tableLayoutPanelVolume";
             this.tableLayoutPanelVolume.RowCount = 1;
-            this.tableLayoutPanelVolume.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanelVolume.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-            this.tableLayoutPanelVolume.Size = new System.Drawing.Size(352, 60);
+            this.tableLayoutPanelVolume.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelVolume.Size = new System.Drawing.Size(319, 75);
             this.tableLayoutPanelVolume.TabIndex = 0;
             // 
             // buttonPlusVolume
             // 
             this.buttonPlusVolume.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.buttonPlusVolume.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonPlusVolume.Location = new System.Drawing.Point(118, 3);
+            this.buttonPlusVolume.Location = new System.Drawing.Point(109, 3);
             this.buttonPlusVolume.Name = "buttonPlusVolume";
-            this.buttonPlusVolume.Size = new System.Drawing.Size(116, 54);
+            this.buttonPlusVolume.Size = new System.Drawing.Size(100, 69);
             this.buttonPlusVolume.TabIndex = 0;
             this.buttonPlusVolume.Text = "Plus";
             this.buttonPlusVolume.UseVisualStyleBackColor = true;
@@ -181,7 +182,7 @@ namespace webBrowser
             this.buttonMinusVolume.Dock = System.Windows.Forms.DockStyle.Fill;
             this.buttonMinusVolume.Location = new System.Drawing.Point(3, 3);
             this.buttonMinusVolume.Name = "buttonMinusVolume";
-            this.buttonMinusVolume.Size = new System.Drawing.Size(109, 54);
+            this.buttonMinusVolume.Size = new System.Drawing.Size(100, 69);
             this.buttonMinusVolume.TabIndex = 1;
             this.buttonMinusVolume.Text = "Minus";
             this.buttonMinusVolume.UseVisualStyleBackColor = true;
@@ -193,9 +194,9 @@ namespace webBrowser
             // 
             this.buttonMuteVolume.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.buttonMuteVolume.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonMuteVolume.Location = new System.Drawing.Point(240, 3);
+            this.buttonMuteVolume.Location = new System.Drawing.Point(215, 3);
             this.buttonMuteVolume.Name = "buttonMuteVolume";
-            this.buttonMuteVolume.Size = new System.Drawing.Size(109, 54);
+            this.buttonMuteVolume.Size = new System.Drawing.Size(101, 69);
             this.buttonMuteVolume.TabIndex = 2;
             this.buttonMuteVolume.Text = "Mute";
             this.buttonMuteVolume.UseVisualStyleBackColor = true;
@@ -207,9 +208,9 @@ namespace webBrowser
             // 
             this.groupBoxRate.Controls.Add(this.tableLayoutPanelRate);
             this.groupBoxRate.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBoxRate.Location = new System.Drawing.Point(950, 3);
+            this.groupBoxRate.Location = new System.Drawing.Point(928, 3);
             this.groupBoxRate.Name = "groupBoxRate";
-            this.groupBoxRate.Size = new System.Drawing.Size(303, 79);
+            this.groupBoxRate.Size = new System.Drawing.Size(242, 94);
             this.groupBoxRate.TabIndex = 4;
             this.groupBoxRate.TabStop = false;
             this.groupBoxRate.Text = "Rate";
@@ -227,16 +228,16 @@ namespace webBrowser
             this.tableLayoutPanelRate.RowCount = 1;
             this.tableLayoutPanelRate.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanelRate.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-            this.tableLayoutPanelRate.Size = new System.Drawing.Size(297, 60);
+            this.tableLayoutPanelRate.Size = new System.Drawing.Size(236, 75);
             this.tableLayoutPanelRate.TabIndex = 0;
             // 
             // buttonPlusRate
             // 
             this.buttonPlusRate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.buttonPlusRate.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonPlusRate.Location = new System.Drawing.Point(151, 3);
+            this.buttonPlusRate.Location = new System.Drawing.Point(121, 3);
             this.buttonPlusRate.Name = "buttonPlusRate";
-            this.buttonPlusRate.Size = new System.Drawing.Size(143, 54);
+            this.buttonPlusRate.Size = new System.Drawing.Size(112, 69);
             this.buttonPlusRate.TabIndex = 0;
             this.buttonPlusRate.Text = "Plus";
             this.buttonPlusRate.UseVisualStyleBackColor = true;
@@ -250,7 +251,7 @@ namespace webBrowser
             this.buttonMinusRate.Dock = System.Windows.Forms.DockStyle.Fill;
             this.buttonMinusRate.Location = new System.Drawing.Point(3, 3);
             this.buttonMinusRate.Name = "buttonMinusRate";
-            this.buttonMinusRate.Size = new System.Drawing.Size(142, 54);
+            this.buttonMinusRate.Size = new System.Drawing.Size(112, 69);
             this.buttonMinusRate.TabIndex = 1;
             this.buttonMinusRate.Text = "Minus";
             this.buttonMinusRate.UseVisualStyleBackColor = true;
@@ -262,9 +263,9 @@ namespace webBrowser
             // 
             this.buttonKeyboard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.buttonKeyboard.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonKeyboard.Location = new System.Drawing.Point(1259, 3);
+            this.buttonKeyboard.Location = new System.Drawing.Point(1176, 3);
             this.buttonKeyboard.Name = "buttonKeyboard";
-            this.buttonKeyboard.Size = new System.Drawing.Size(188, 79);
+            this.buttonKeyboard.Size = new System.Drawing.Size(192, 94);
             this.buttonKeyboard.TabIndex = 5;
             this.buttonKeyboard.Text = "Keyboard";
             this.buttonKeyboard.UseVisualStyleBackColor = true;
@@ -272,12 +273,26 @@ namespace webBrowser
             this.buttonKeyboard.MouseEnter += new System.EventHandler(this.buttons_MouseEnter);
             this.buttonKeyboard.MouseLeave += new System.EventHandler(this.buttons_MouseLeave);
             // 
+            // buttonClickMouse
+            // 
+            this.buttonClickMouse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonClickMouse.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonClickMouse.Location = new System.Drawing.Point(1374, 3);
+            this.buttonClickMouse.Name = "buttonClickMouse";
+            this.buttonClickMouse.Size = new System.Drawing.Size(165, 94);
+            this.buttonClickMouse.TabIndex = 6;
+            this.buttonClickMouse.Text = "Click";
+            this.buttonClickMouse.UseVisualStyleBackColor = true;
+            this.buttonClickMouse.Click += new System.EventHandler(this.buttonClickMouse_Click);
+            this.buttonClickMouse.MouseEnter += new System.EventHandler(this.buttons_MouseEnter);
+            this.buttonClickMouse.MouseLeave += new System.EventHandler(this.buttons_MouseLeave);
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::webBrowser.Properties.Resources._0;
-            this.ClientSize = new System.Drawing.Size(1450, 654);
+            this.ClientSize = new System.Drawing.Size(1542, 669);
             this.Controls.Add(this.mainTableLayoutPanel);
             this.Name = "MainWindow";
             this.Text = "Browser";
@@ -307,6 +322,7 @@ namespace webBrowser
         private System.Windows.Forms.Button buttonPlusRate;
         private System.Windows.Forms.Button buttonMinusRate;
         private System.Windows.Forms.Button buttonKeyboard;
+        private System.Windows.Forms.Button buttonClickMouse;
     }
 }
 
--- webBrowser/webBrowser/Form1.cs
+++ webBrowser/webBrowser/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace webBrowser
 {
@@ -20,6 +21,13 @@ namespace webBrowser
         System.Diagnostics.Process keyboardProc;
 
         ImageList imageListProgressButton = new ImageList();
+
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
 
         public MainWindow()
         {
@@ -220,6 +228,24 @@ namespace webBrowser
             }            
         }
 
+        private void buttonClickMouse_Click(object sender, EventArgs e)
+        {
+            Thread threadClickMouse = new Thread(new ThreadStart(delegate
+            {
+                Thread.Sleep(2000);
+                DoMouseClick();
+            }));
+            threadClickMouse.Start();
+        }
+
+        public void DoMouseClick()
+        {
+            //Call the imported function with the cursor's current position
+            uint X = (uint)Cursor.Position.X;
+            uint Y = (uint)Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+
         private void setImageInButton(Button btn, ImageList imgList)
         {
             ImageList cpyImgList = imgList;
@@ -295,5 +321,7 @@ namespace webBrowser
                 "setInterval(scrollPage, 50);");
             head.AppendChild(s);
         }
+
+        
     }
 }
